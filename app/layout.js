2596cebdf3a9c826ce6edf8a7b372a6c07b87926
Andrii_Kuzhelnii <remontdiskov1979@gmail.com>
2024-02-26 00:51:15 +0200

    ad vercel speed

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header/header";
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
 			<body className={inter.className}>
 				<Header />
 				{children}
+        <SpeedInsights/>
 			</body>
 		</html>
 	);
